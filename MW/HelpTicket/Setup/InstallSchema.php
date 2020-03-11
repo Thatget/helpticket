54c1babd3e1792a924d547ed244481d60c9fc44b
thatget <quyet.nguyen.7712826gmail.com>
2020-03-12 00:30:28 +0700

    try

--- MW/HelpTicket/Setup/InstallSchema.php
+++ MW/HelpTicket/Setup/InstallSchema.php
@@ -15,11 +15,113 @@ class InstallSchema implements InstallSchemaInterface{
         $installer = $setup;
         $installer->startSetup();
         if (!$installer->tableExists('mw_table_helpticket')){
-            $table = $installer->getConnection()->newTable(
+            $table1 = $installer->getConnection()->newTable(
                 $installer->getTable('mw_table_helpticket')
             )->addColumn(
-                
-            )
+                'ticket_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Ticket Id'
+            )->addColumn(
+                    'customer_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['unsigned' => true],
+                    'Customer Id'
+            )->addColumn(
+                    'title',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false],
+                    'Title'
+                )
+                ->addColumn(
+                    'severity',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false],
+                    'Severity'
+                )
+                ->addColumn(
+                    'created_at',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                    null,
+                    ['nullable' => false],
+                    'Created At'
+                )
+                ->addColumn(
+                    'status',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false],
+                    'Severity'
+                );
+
+            $installer->getConnection()->createTable($table1);
         }
+        if (!$installer->tableExists('mw_helpticket_department')){
+            $table2 = $installer->getConnection()->newTable(
+                $installer->getTable('mw_helpticket_department')
+            )->addColumn(
+                'department_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Department Id'
+            )->addColumn(
+                    'title',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false],
+                    'Title'
+            )->addColumn(
+                'staff',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'staff'
+            )->addColumn(
+                    'status',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false],
+                    'Severity'
+                );
+
+            $installer->getConnection()->createTable($table2);
+        }
+        if (!$installer->tableExists('mw_helpticket_topic')){
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('mw_helpticket_topic')
+            )->addColumn(
+                'topic_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'topic Id'
+            )->addColumn(
+                'title',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                null,
+                ['nullable' => false],
+                'Title'
+            )->addColumn(
+                'customer_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false],
+                'Customer Id'
+            )->addColumn(
+                'status',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false],
+                'Severity'
+            );
+
+            $installer->getConnection()->createTable($table);
+        }
+        $installer->endSetup();
     }
 }
